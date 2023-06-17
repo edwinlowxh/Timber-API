48cb7f0aa818fa5e187c8e2ede8a4a2f6c02cce9
edwin <edwin.lowxh@gmail.com>
2023-06-17 22:23:54 +0800

    Updated class name

--- src/main/java/org/whitestar/TimberDemo/repository/RequestRepository.java
+++ src/main/java/org/whitestar/TimberDemo/repository/RequestRepository.java
@@ -2,12 +2,8 @@ package org.whitestar.TimberDemo.repository;
 
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import org.whitestar.TimberDemo.entity.Request;
-import org.whitestar.TimberDemo.entity.SkillTypeEntity;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 
 @EnableScan
