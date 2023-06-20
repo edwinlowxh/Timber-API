c875e6a85020fe49ca918116354d206e50924e09
edwin <edwin.lowxh@gmail.com>
2023-06-20 19:56:58 +0800

    Added news component

---
+++ src/main/java/org/whitestar/TimberDemo/repository/NewsRepository.java
@@ -0,0 +1,9 @@
+package org.whitestar.TimberDemo.repository;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+import org.whitestar.TimberDemo.entity.News;
+
+@EnableScan
+public interface NewsRepository extends CrudRepository<News, String> {
+}
